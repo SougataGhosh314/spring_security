e52bac96182df3facfec6fe9e2e83204d770a712
Sougata Ghosh <sougataghosh314@gmail.com>
2021-02-04 23:49:23 +0530

    spring security

---
+++ src/main/java/com/sougata/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.sougata.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //  set my own configuration on the auth object
+
+        auth.inMemoryAuthentication()
+                .withUser("dummy")
+                .password("qwerty")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("qwerty")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}
+//
+//        http.authorizeRequests()
+//                .antMatchers("/", "static/css", "static/js").permitAll()
+//                .antMatchers("/**").hasRole("ADMIN")
+//                .and().formLogin();
---
+++ src/test/java/com/sougata/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sougata.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
